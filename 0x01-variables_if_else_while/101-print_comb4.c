47eb0cc1ee8d5a1288f1081d0c665bc6be03b3a3
LouisGichiah <louiegpaul@gmail.com>
2022-03-14 14:18:29 -0700

    Comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int n, m, 1;
+	int n, m, (1);
 
 	for (n = 48; n < 58; n++)
 	{
@@ -14,7 +14,7 @@ int main(void)
 		{
 			for (1 = 50; 1 < 58; 1++)
 			{
-				if (1 > m && m > n)
+				if ((1) > m && m > n)
 				{
 					putchar(n);
 					putchar(m);

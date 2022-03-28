922021fbad614b7897b76632b405f1285b2e7f22
LouisGichiah <louiegpaul@gmail.com>
2022-03-28 05:02:57 -0700

    Strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,12 +19,12 @@ char *_strcat(char *dest, char *src)
 
 	while (src[j] != '\0')
 	{
-		dest[1] = src[j];
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
 
-	dest[i] = '\0':
+	dest[i] = '\0';
 
 	return (dest);
 }

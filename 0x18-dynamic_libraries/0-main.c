a34cf6fcf98a124af78d80a9dfd53e2b484d4048
LouisGichiah <louiegpaul@gmail.com>
2022-06-15 14:35:18 -0700

    DEbugging

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,12 @@
+#include "../holberton.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code for Holberton Students
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n, _strlen("Holberton"));
+	return (EXIT_SUCCESS);
+}
